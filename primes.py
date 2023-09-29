4c0ad199482b38a9c70fce2a1c426f3f3eaa7806
benx9802 <benjamin.ferrey@kcl.ac.uk>
2023-09-29 20:58:30 +0100

    Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
-    list = []
-    return list
+    try:
+        if number_of_primes <= 0:
+            raise ValueError
+        list = []
+        x = 2
+        while len(list) < number_of_primes:
+            if ifprime(x):
+                list.append(x)
+            x = x+1
+        return list
+    except ValueError:
+        print(f'Cannot use {number_of_primes}, must use a positive integer!')
+        raise
+
+
+def ifprime(num):
+    for i in range(2,num): # for all possible factors of i
+        if num % i == 0: # if it is a factor
+            return False
+    return True
